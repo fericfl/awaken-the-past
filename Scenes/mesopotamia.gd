fdb92de85ec1fa8fad4ed72aed3ec4d5bc043e52
Eric Florea <eric.florea@student.upt.ro>
2024-11-09 22:15:06 +0200

    "Refined menu and added mesopotamia intro"

--- Scenes/mesopotamia.gd
+++ Scenes/mesopotamia.gd
@@ -7,11 +7,11 @@ func _ready() -> void:
 func start_dialog():
     # Connect to the timeline_ended signal
     Dialogic.timeline_ended.connect(_on_timeline_ended)
-    Dialogic.start("classroom")
+    Dialogic.start("mesopotamia")
 
 func _on_timeline_ended():
     # Disconnect the signal to prevent it from firing multiple times
     Dialogic.timeline_ended.disconnect(_on_timeline_ended)
     
     # Change to the new scene
-    get_tree().change_scene_to_file("res://Scenes/mesopotamia.tscn")
+    get_tree().change_scene_to_file("res://Scenes/control_page.tscn")
